72c7bdb1fb6abf839823969adfaa75f4ffc504e8
Marijn Verbeek <marijn.verbeek@ritense.com>
2026-04-10 13:39:24 +0200

    Updated imports to add support for custom translations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {Injector, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {HttpBackend, HttpClientModule} from '@angular/common/http';
+import {HttpBackend, HttpClient, HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LayoutModule, TranslationManagementModule} from '@valtimo/layout';
@@ -53,7 +53,12 @@ import {ProcessLinkModule} from '@valtimo/process-link';
 import {MigrationModule} from '@valtimo/migration';
 import {DossierManagementModule} from '@valtimo/dossier-management';
 import {BootstrapModule} from '@valtimo/bootstrap';
-import {ConfigModule, ConfigService, MultiTranslateHttpLoaderFactory} from '@valtimo/config';
+import {
+  ConfigModule,
+  ConfigService,
+  CustomMultiTranslateHttpLoaderFactory,
+  LocalizationService,
+} from '@valtimo/config';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {FormFlowManagementModule} from '@valtimo/form-flow-management';
 import {PluginManagementModule} from '@valtimo/plugin-management';
@@ -176,8 +181,8 @@ export function tabsFactory() {
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: MultiTranslateHttpLoaderFactory,
-        deps: [HttpBackend, ConfigService]
+        useFactory: CustomMultiTranslateHttpLoaderFactory,
+        deps: [HttpBackend, HttpClient, ConfigService, LocalizationService],
       },
     }),
     TranslationManagementModule,
