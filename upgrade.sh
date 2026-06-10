fc5f362cf2e963fb0bdcb1497d7f911545dfcba5
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-06-10 16:10:45 +0200

    Make upgrade.sh sed call portable across GNU and BSD

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git fetch && git checkout v/${VALTIMO_RELEASE_MAJOR_VERSION} && git pull
-sed -r -i '' 's/"@valtimo\/([a-z0-9_-]+)".+/"@valtimo\/\1": "'${VALTIMO_RELEASE_VERSION}'",/' package.json
+sed -E -i.bak 's/"@valtimo\/([a-z0-9_-]+)".+/"@valtimo\/\1": "'${VALTIMO_RELEASE_VERSION}'",/' package.json && rm -f package.json.bak
 npm i
 git add package.json && git add package-lock.json && git commit -m "Upgrade to ${VALTIMO_RELEASE_VERSION}" && git push
